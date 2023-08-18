a0c0a99c7229c473dc91f3cc5751c59a8ea4c622
BaMoustapha <127317481+BaMoustapha@users.noreply.github.com>
2023-08-18 22:52:16 +0000

    Add files via upload

---
+++ dom2/index.js
@@ -0,0 +1,40 @@
+const div1 = document.querySelector('.box1');
+div1.addEventListener('mouseover', () => {
+  div1.classList.add('border');
+});
+
+div1.addEventListener('mouseout', () => {
+  div1.classList.remove('border');
+});
+
+
+const div2 = document.querySelector('.box2');
+div2.addEventListener('mouseover', () => {
+  div2.classList.add('border');
+});
+
+div2.addEventListener('mouseout', () => {
+  div2.classList.remove('border');
+});
+
+
+
+const div3 = document.querySelector('.box3');
+div3.addEventListener('mouseover', () => {
+  div3.classList.add('border');
+});
+
+div3.addEventListener('mouseout', () => {
+  div3.classList.remove('border');
+});
+
+// -------------------------------------
+// quatriéme border
+const div4 = document.querySelector('.box4');
+div4.addEventListener('mouseover', () => {
+  div4.classList.add('border');
+});
+
+div4.addEventListener('mouseout', () => {
+  div4.classList.remove('border');
+});

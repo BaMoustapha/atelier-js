db78834f8f9da4ac2ea66532c6c4fb65acc3e967
BaMoustapha <127317481+BaMoustapha@users.noreply.github.com>
2023-10-04 23:44:48 +0000

    Add files via upload

---
+++ 30days/day1/variables.js
@@ -0,0 +1,17 @@
+let firstName = 'Asabeneh'   
+let lastName = 'Yetayeh'      
+let city = 'Helsinki'         
+let age = 100                 
+let isMarried = true
+
+// Declaring variables with number values
+
+const gravity = 9.81         
+const boilingPoint = 100     
+const PI = 3.14             
+
+// Variables can also be declaring in one line separated by comma
+
+let name = 'Asabeneh',     
+  job = 'teacher',
+  live = 'Finland'
